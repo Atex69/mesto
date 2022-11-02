b3b63f2a2c1c903a5ff2f702eef99a5f3fa649ce
Emil Bayanov <atex-69@mail.ru>
2022-11-02 11:35:00 +0300

    5

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 let editButton = document.querySelector('.profile__info-editButton');
-let addButton = document.querySelector('.profile__addButton');
-let like = document.querySelector('.element__like');
 let popup =  document.querySelector('.popup');
 let popupCloseButton =  document.querySelector('.popup__close');
 let form = document.querySelector('.popup__form');
@@ -15,19 +13,6 @@ function popupClose() {
     popup.classList.remove('popup_opened');
 }
 
-function likeRender() {
-    if(like.classList.contains("element__like_active")){
-        like.classList.remove('element__like_active');
-    } else {
-        like.classList.add('element__like_active');
-    }
-}
 
-function addLike(){
-    like.classList.add('element__like_active')
-}
-
-
-like.addEventListener('click', likeRender);
 editButton.addEventListener('click', popupOpen);
 popupCloseButton.addEventListener('click', popupClose);
