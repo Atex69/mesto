5c263650e3b13e4f02d3521107a8b1d1e22ebe5e
Emil Bayanov <atex-69@mail.ru>
2023-01-27 14:12:41 +0300

    start 9 work

---
+++ src/components/Api.js
@@ -0,0 +1,9 @@
+const apiData = {
+    url: "https://nomoreparties.co/v1/cohort-57",
+    headers: {
+        authorization: "ad54961e-3f85-45c8-ba7a-70af841df2af",
+        "Content-Type": "application/json",
+    },
+};
+const fapi = new Api(apiData);
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -18,15 +18,6 @@ import {
 
 import "./index.css";
 
-const apiData = {
-    url: "https://nomoreparties.co/v1/cohort-57",
-    headers: {
-        authorization: "ad54961e-3f85-45c8-ba7a-70af841df2af",
-        "Content-Type": "application/json",
-    },
-};
-const fapi = new Api(apiData);
-
 
 const userInfo = new UserInfo({
     nameSelector: ".profile__info-title",
