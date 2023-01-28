32a9e82f98139439a7f22129d92cd457ca045693
Emil Bayanov <atex-69@mail.ru>
2023-01-28 22:12:24 +0300

    update: finish2

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -29,13 +29,8 @@ export default class PopupWithForm extends Popup {
   setEventListeners() {
     super.setEventListeners();
     this._form.addEventListener("submit", (e) => {
-      if (this._submitButton.classList.contains("popup__submit_disabled")) {
-        return false;
-      } else {
         e.preventDefault();
-        this._submitHandler(this._getInputValues());
-        this.close();
-      }
+        this._submitHandler(this._getInputValues())
     });
   }
 
